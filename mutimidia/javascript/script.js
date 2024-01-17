153ab37b184691fe786ebfbcfd299e1b12936443
Hanayel Prasilde <64213838+Hanayel@users.noreply.github.com>
2024-01-16 22:47:06 -0300

    Update script.js

--- mutimidia/javascript/script.js
+++ mutimidia/javascript/script.js
@@ -1,1 +1,14 @@
+const imgs = document.getElementById("img_carrossel");
+const img = document.querySelectorAll("#img_carrossel img");
 
+let fluxo = 0;
+function carrossel(){
+    fluxo++;
+    if(fluxo > img.length - 1){
+        fluxo = 0;
+    }
+
+    imgs.style.transform ='translateX(${-fluxo * 500}px)';
+
+}
+setInterval(carrossel, 1800);
